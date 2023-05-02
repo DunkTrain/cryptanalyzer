9ee1763c10d8801e00bfe7b843a5c3e3b36b1b5a
Dmitry Shevchenko <duosora1997@gmail.com>
2023-05-02 22:13:54 +0300

    Create CryptoAlphabet.java, EntryPoint.java and FileConstants.java

---
+++ src/main/java/com/javarush/cryptanalyzer/shevchenko/app/EntryPoint.java
@@ -0,0 +1,10 @@
+package com.javarush.cryptanalyzer.shevchenko.app;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class EntryPoint {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/main/java/com/javarush/cryptanalyzer/shevchenko/constants/FileConstants.java
@@ -0,0 +1,8 @@
+package com.javarush.cryptanalyzer.shevchenko.constants;
+
+public class FileConstants {
+
+    public static final String INPUT = "input.txt";
+    public static final String OUTPUT = "output.txt";
+    public static final String ENCODED = "encoded.txt";
+}
