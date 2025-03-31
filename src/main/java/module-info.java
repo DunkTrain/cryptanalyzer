a84489cfd583f6a06881a17cfb98ead46648e543
Шевченко Дмитрий <d.shevchenko@ylab.team>
2025-03-31 16:38:56 +0300

    v2.1.0 Изменения pom для корректной сборки проекта и дальнейшего запуска пользователем
    - Создание модульности проекта module-info.java
    - Добавил jlink в pom.xml для сборки проекта
    - Обновлена README.md

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module cryptanalyzer {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    exports ru.cooper.cryptanalyzer;
+
+    opens ru.cooper.cryptanalyzer.controllers.ui to javafx.fxml;
+}
